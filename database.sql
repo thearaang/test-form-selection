66e1b3cc7b36b9df2ab3200b5ecbda39de8c23ac
Ang Theara <theara.ang@codingate.com>
2022-03-07 23:03:56 +0700

    [TASK] Add database

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `phppos_attribute_item_gold` (
+  `id` int(10) NOT NULL,
+  `attribute_gold` int(10) NOT NULL,
+  `catogory_id` int(10) NOT NULL DEFAULT 0,
+  `deleted` int(10) NOT NULL DEFAULT 0,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (1);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (2);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (3);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (4);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (5);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (6);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (7);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (8);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (9);
+INSERT INTO `phppos_attribute_item_gold`(`attribute_gold`) VALUES (10);
